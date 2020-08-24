0cc8bbdc4b1f23f5916955c4ec2383480622c47d
Pritish Mishra <pritish.mishra94@gmail.com>
2020-08-24 14:23:00 +0530

    Implementing ML classifier in the flask app

--- workspace/models/train_classifier.py
+++ workspace/models/train_classifier.py
@@ -1,24 +1,70 @@
 import sys
+# import libraries
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy as np
+import nltk
+import re
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.externals import joblib
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
 
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('InsertTableName', engine)
+    df = df.dropna(subset=df.select_dtypes(float).columns, how='all')
+    X = df.message.values
+    Y = df[df.columns[4:]].values
+    category_names = df.columns[4:]
+    return (X, Y, category_names)
 
 
+special_char_regex = '[^\w\*]'
 def tokenize(text):
-    pass
+    special_chars = re.findall(special_char_regex, text)
+    for special_char in special_chars:
+        text = text.replace(special_char, " ")
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+    return clean_tokens
+    
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer()),
+                ('clf', MultiOutputClassifier(KNeighborsClassifier()))
+        ])
+    return pipeline
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    Y_pred = model.predict(X_test)
+    accuracy = (Y_pred == Y_test).mean()
+    print("Accuracy:", accuracy)
+    for i in range(36):
+        print("Statistics for category: ", category_names[i], "\n", classification_report(Y_test[:,i],Y_pred[:,i]))
 
 
 def save_model(model, model_filepath):
-    pass
+    # Save the model as a pickle in a file 
+    joblib.dump(model, model_filepath)
 
 
 def main():
